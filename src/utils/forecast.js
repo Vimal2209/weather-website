eb418776f7ac0ac9b572195a406311e912c0e4ff
Vimal Kumar <vimalskr97@gmail.com>
2020-04-26 20:53:05 +0530

    Added some data for forecast.js to display in browser

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, long , callback)=>{
         }else if(body.error){
             callback('URL passed is incorrect', undefined)
         }else{
-            callback(undefined,  'Its is currently ' + body.current.weather_descriptions[0] + ' of the temperature '+body.current.temperature+' and it feels like'+body.current.feelslike)
+            callback(undefined,  body.current.weather_descriptions[0]+'. It is currently '+ body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out. The humidity is '+body.current.humidity+'%')
         }
     })
 }
